c04e9cd5303aeea038af89895c923312149cbe47
Aeliasceledon <aeliasceledon@uniguajira.edu.co>
2020-12-13 23:25:42 -0500

    version base de la interfaz de login

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+//import store from "./store"; //esto lo añadí por que así aparece en la guia de imaster pero acá me da error
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+
+const base = axios.create({
+    baseURL: "http://localhost:3000"
+});
+
+Vue.prototype.$http = base;
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    //store,
+    render: h => h(App)
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -5,24 +5,41 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
+    {
+        path: "/home",
+        name: "Home",
+        component: Home,
+        meta: {
+            requiresAuth: true
+        }
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    {
+        path: "/",
+        name: "login",
+        component: () =>
+            //import("../views/login.vue") //otro error en la guia
+            import("../components/auth/login.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem("jwt") == null) {
+            next({
+                path: "/"
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
